64b039006cbb9a3828c1043c11be115f81fd51d2
Anna Lewis Dagnino <annaick1995@gmail.com>
2019-11-21 16:03:22 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -4,8 +4,13 @@ require 'directors_database'
 
 def pretty_print_nds(nds)
   # Change the code below to pretty print the nds with pp
+  pp nds
   nil
 end
 
 def print_first_directors_movie_titles
+  
+  pp directors_database[0][:movies].each {|key, value| puts index}
 end
+
+#pretty_print_nds(directors_database)
