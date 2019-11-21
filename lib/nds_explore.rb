423107f27cf3e0d99152b94a99e16347dbaba8a5
Anna Lewis Dagnino <annaick1995@gmail.com>
2019-11-21 16:04:35 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -10,7 +10,7 @@ end
 
 def print_first_directors_movie_titles
   
-  pp directors_database[0][:movies].each {|key, value| puts index}
+  directors_database[0][:movies].each {|key, value| puts index}
 end
 
 #pretty_print_nds(directors_database)
