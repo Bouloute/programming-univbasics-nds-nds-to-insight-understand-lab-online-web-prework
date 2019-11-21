f129c487c6512bbc8c15481ca2180654d139d01d
Anna Lewis Dagnino <annaick1995@gmail.com>
2019-11-21 16:07:48 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -10,7 +10,7 @@ end
 
 def print_first_directors_movie_titles
   
-   pp directors_database[0][:movies].each {|key, value| if key == :title puts value}
+   pp directors_database[0][:movies].each {|key, value| if key == :title puts value end}
 end
 
 #pretty_print_nds(directors_database)
